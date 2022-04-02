057204f4b3e48555000ab3157eaacf047970062f
Sergii Zasenko <sergii@zasenko.name>
2022-04-02 13:04:41 +0300

    backward compatibility modules

---
+++ contour-tracing.js
@@ -0,0 +1,3 @@
+// Left for backward compatability, please do not rely on it
+const { ContourTracing } = require('./lib')
+module.exports = ContourTracing
---
+++ utils.js
@@ -0,0 +1,3 @@
+// Left for backward compatability, please do not rely on it
+const { SVG, EPS, PNGImageData } = require('./lib')
+module.exports = { SVG, EPS, PNGImageData }
